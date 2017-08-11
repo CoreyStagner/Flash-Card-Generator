acb5c1e1adcd7ccd37af3deda36a1548dd6602d6
Corey R. Stagner <cresc0428@gmail.com>
2017-08-10 22:15:50 -0400

    working, still making minor changes

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,19 +1,21 @@
-function Card(front, back) {
+function ClozeCard(front, back) {
 // Card Constructor
-    this.front = front;
+    this.full = front;
     this.back = back;
+    this.front = this.full.replace(this.back, "_______");
     // Card.recordCard allows the user to record the users own questions and push 
     // them to the questions array to use when running playFlashCards().
     this.recordCard = function(front, back){
         data = {
-            type: "basic",
-            front: this.front,
+            type: "cloze",
+            full: this.front,
+            front: this.front, // needs to be fixed
             back: this.back
-        } //end data{}
+        }; //end data{}
         var newCard = data;
         questionsArr.push(data);
         console.log(questionsArr);
     }//end recordCard()
 }//end Card({})
 
-module.exports = Card
+module.exports = ClozeCard
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require("fs"); // Allows user to access the file system surrounding the
 var inquire = require("inquirer"); // Allows the user to interact with the app and have the app see the users input
 var chalk = require("chalk"); // Allows adding color to text in the CLI
 var Card = require("./Card"); // Imports the Card Constructor
+var ClozeCard = require("./ClozeCard.js"); // Imports the Cloze Card Constructor
 
 // Initial Values
 var questionsArr = []; // Creates an empty array
@@ -11,6 +12,8 @@ var score = 0; // Declares and set the variable to zero for the first run
 var wrong = 0; // Declares and set the variable to zero for the first run
 
 // Functions
+
+
 
 function getQuestions(){
 // Function used to get the questions[] from the library.json file
@@ -84,7 +87,82 @@ function gameChoice(){
             console.log("You are studying your flash cards. Now you will ace this test.");
             playFlashCards();
         } else if(rg === "Create New Cards"){
-            console.log("Let's make a new card");
+            inquire.prompt([
+                {
+                    type: "list",
+                    message: "What kind of card would you like to create?",
+                    choices: ["Q & A", "Fill in the blank"],
+                    name: "cardType"
+                }
+            ]).then(function(response){
+                var newCardType = response.cardType;
+                console.log(newCardType);
+                if(newCardType === "Q & A"){
+                    cardCreate();
+                } else if (newCardType === "Fill in the blank"){
+                    clozeCardCreate();
+                } else {
+                    console.log("Stop Hammertime");
+                }
+            });
+            // cardCreate();
+            // console.log("Let's make a new card");
+            // inquire.prompt([
+            //     {
+            //         type: "input",
+            //         message: "What is your question?",
+            //         name: "front"
+            //     }
+            // ]).then(function(response){
+            //     front = response.front;
+            //     console.log("You just submitted this front: \n" + front);
+            //     inquire.prompt([
+            //         {
+            //             type: "input",
+            //             message: "What is answer to your question?",
+            //             name: "back"
+            //         }
+            //     ]).then(function(input){
+            //         var back = input.back;
+            //         // console.log("back is " + back);
+            //         // console.log("You just submitted this back: \n" + back);
+            //         var newCard = new Card(front, back);
+            //         questionsArr.push(newCard);
+            //         setQuestions();
+            //         gameChoice();
+            //     });// end answer inquire.prompt.then()
+            // });// end question inquire.prompt.then()
+        } else if(rg === "Clear All Flash Cards"){
+            clearQuestions();
+        } else if(rg === "Quit school and be a bum!"){
+            console.log(chalk.inverse("That is a personal decision, but I am going to recommend against that!"));
+        } else{
+            console.log("I have no idea how you got here!");
+        }// end else if()
+    }); // end .then()
+}// end gameChoice()
+
+function pickCard(){
+    var questionsRemaining = questionsArr.length;
+    var randomIndex = Math.floor(Math.random() * questionsRemaining);
+    var chosenCard = questionsArr[randomIndex];
+    // console.log(chosenCard);
+    questionsArr.splice(randomIndex, 1);
+    return chosenCard;
+}// end pickCard()
+
+function clearQuestions(){
+    questionsArr = [];
+    fs.writeFile("library.json", "[]", function(err){
+        if(err){
+            console.log("Error: " + err);
+        }//end if();
+    })
+    gameChoice();
+};
+
+function cardCreate(){
+                console.log("Let's make a new card");
             inquire.prompt([
                 {
                     type: "input",
@@ -108,38 +186,40 @@ function gameChoice(){
                     questionsArr.push(newCard);
                     setQuestions();
                     gameChoice();
-    
                 });// end answer inquire.prompt.then()
-            });// end question inquire.prompt.then()
-        } else if(rg === "Clear All Flash Cards"){
-            clearQuestions();
-        } else if(rg === "Quit school and be a bum!"){
-            console.log(chalk.inverse("That is a personal decision, but I am going to recommend against that!"));
-        } else{
-            console.log("I have no idea how you got here!");
-        }// end else if()
-    }); // end .then()
-}// end gameChoice()
-
-function pickCard(){
-    var questionsRemaining = questionsArr.length;
-    var randomIndex = Math.floor(Math.random() * questionsRemaining);
-    var chosenCard = questionsArr[randomIndex];
-    // console.log(chosenCard);
-    questionsArr.splice(randomIndex, 1);
-    return chosenCard;
-}// end pickCard()
-
-function clearQuestions(){
-    fs.writeFile("library.json", "[]", function(err){
-        if(err){
-            console.log("Error: " + err);
-        }//end if();
-    })
-    gameChoice();
+            });
 }
-// Sandbox
 
-// setQuestions();
-// getQuestions();
+function clozeCardCreate(){
+    console.log("Started Cloze Card Construction");
+                console.log("Let's make a new card");
+            inquire.prompt([
+                {
+                    type: "input",
+                    message: "What is the full text?",
+                    name: "front"
+                }
+            ]).then(function(response){
+                front = response.front;
+                inquire.prompt([
+                    {
+                        type: "input",
+                        message: "What would you like hidden?",
+                        name: "back"
+                    }
+                ]).then(function(input){
+                    var back = input.back;
+                    // console.log("back is " + back);
+                    // console.log("You just submitted this back: \n" + back);
+                    var newCard = new ClozeCard(front, back);
+                    questionsArr.push(newCard);
+                    setQuestions();
+                    gameChoice();
+                });// end answer inquire.prompt.then()
+    // var newCard = new ClozeCard("Pickles is Kevins Favorite Variable", "Pickles");
+    // questionsArr.push(newCard);
+    // setQuestions();
+            });
+}// end clozeCardCreate()
+
 gameChoice();
